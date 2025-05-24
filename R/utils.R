653f3ec2672cd1df3755c4b91de2ab825f0bc2c3
Ethan Bass <ethanbass@gmail.com>
2025-05-24 10:18:46 -0500

    feat: added `source` argument to `check_for_pkg`
    so it can tell users to download packages from bioconductor rather than CRAN

--- R/utils.R
+++ R/utils.R
@@ -6,11 +6,17 @@
 #'
 #' @noRd
 
-check_for_pkg <- function(pkg, return_boolean = FALSE){
+check_for_pkg <- function(pkg, return_boolean = FALSE,
+                          source = c("cran", "bioconductor")){
+  source <- match.arg(source, c("cran", "bioconductor"))
   pkg_exists <- requireNamespace(pkg, quietly = TRUE)
   if (return_boolean){
     return(pkg_exists)
   } else if (!pkg_exists) {
+    suggestion <- switch(source, "cran" = sprintf("`install.packages('%s')`", pkg),
+                         "bioconductor" = sprintf("`BiocManager::install('%s')`", pkg))
+    stop(sprintf("Package `%s` must be installed to perform this action:
+                 try `%s`", pkg, suggestion))
     stop(paste(
       "Package", sQuote(pkg), "must be installed to perform this action:
           try", paste0("`install.packages('", pkg, "')`.")),
